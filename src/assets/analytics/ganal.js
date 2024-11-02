506f4c9c02c02c071aed489d7282a2a18eb784d6
Anatolii Zorin <Zorger27@GMail.com>
2024-11-03 00:50:03 +0200

    Small changes

--- src/assets/analytics/ganal.js
+++ src/assets/analytics/ganal.js
@@ -1,4 +1,4 @@
-// window.dataLayer = window.dataLayer || [];
-// function gtag(){dataLayer.push(arguments);}
-// gtag('js-server', new Date());
-// gtag('config', 'G-RVMP917YXN');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-PE45QS2FDR');
